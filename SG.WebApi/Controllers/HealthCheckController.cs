e609e4360111fb51bcd114b2c1b7416556dfdd21
stoyangadev <stoyangadev@coherentsolutions.com>
2021-05-13 15:19:34 +0300

    initial commit

---
+++ SG.WebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SG.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return new JsonResult("Application is up and running ...");
+        }
+    }
+}
